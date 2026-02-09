375a91f425d3136ed7374eccb47f529299ec33fe
Yahya <eksayahya92@gmail.com>
2026-02-09 11:50:43 +0700

    second commit python project

--- taskOne.py
+++ taskOne.py
@@ -1,8 +1,12 @@
+# Inialisasi Batas Array pada Program [Fixed-Size Array]
 Max = 10
+# Membuat [Fixed-size Array] dengan nilai awal kosong
 arrayNIMMahasiwa = [None] * Max
 arrayNamaMahasiwa = [None] * Max
 
+# Variable untuk melacak kondisi jumlah data
 n = 0
+
 while True:
     print("\n ======== MENU ========")
     print("1. Masukkan Data Pada Index Pertama")
@@ -19,21 +23,22 @@ while True:
 
     match pilihan:
 
+        # Insert At Beginning
         case 1: 
             inpNIMMahasiswa = int(input("Masukkan NIM Mahasiswa: "))
             inpNamaMahasiswa = input("Masukkan Nama Mahasiswa: ")
-
 
             if n < Max:
                 for i in range(n,0,-1):
                     arrayNIMMahasiwa[i] = arrayNIMMahasiwa[i-1]
                     arrayNamaMahasiwa[i] = arrayNamaMahasiwa[i-1]
-            # Insert
+
             arrayNIMMahasiwa[0] = inpNIMMahasiswa
             arrayNamaMahasiwa[0] = f"Nama: {inpNamaMahasiswa}"
             n+=1
             print("Data Berhasil Ditambahkan :)")
 
+        # Insert At Given Position
         case 2: 
             inpIndexMahasiswa = int(input(f"Masukkan Index: (0-{n}): "))
             if n < Max and 0 <= inpIndexMahasiswa <= n:
@@ -45,50 +50,56 @@ while True:
                     arrayNamaMahasiwa[i] = arrayNamaMahasiwa[i-1]
                 
                 arrayNIMMahasiwa[inpIndexMahasiswa] = inpNIMMahasiswa
-                arrayNamaMahasiwa[inpIndexMahasiswa] = inpNamaMahasiswa
+                arrayNamaMahasiwa[inpIndexMahasiswa] = f"Nama: {inpNamaMahasiswa}"
                 n+=1
                 print(f"Data Berhasil Ditambahkan pada Index {inpIndexMahasiswa}")
             else:
                 print(f"Index pada {inpIndexMahasiswa}, Gagal Ditambahkan")
+
+        # Insert At End
         case 3:
             if n < Max:
                 inpNIMMahasiswa = int(input("Masukkan NIM Mahasiswa: "))
                 inpNamaMahasiswa = input("Masukkan Nama Mahasiswa: ")
 
                 arrayNIMMahasiwa[n] = inpNIMMahasiswa
-                arrayNamaMahasiwa[n] = inpNamaMahasiswa
+                arrayNamaMahasiwa[n] = f"Nama: {inpNamaMahasiswa}"
                 n += 1
+
+                print("Data Berhasil Ditambahkan")
             else:
                 print("Data Gagal Ditambahkan")
 
+        # Delete From Beginning
         case 4:
             if n > 0:
                 for i in range(0,n -1):
                     arrayNIMMahasiwa[i] = arrayNIMMahasiwa[i+1]
                     arrayNamaMahasiwa[i] = arrayNamaMahasiwa[i+1]
 
-                arrayNIMMahasiwa[n-1] = None
-                arrayNamaMahasiwa[n-1] =None
+                arrayNIMMahasiwa[n-1]  = None
+                arrayNamaMahasiwa[n-1] = None
                 n -=1
                 print("Data Berhasil Dihapus")
             else:
                 print("Gagal Dihapus")
 
+        # Delete Given Position
         case 5:
-            inpIndexDeleteMahasiswa = int(input(f"Masukkan Index: [0-{n-1}]"))
-
+            inpIndexDeleteMahasiswa = int(input(f"Masukkan Index [0-{n-1}]: "))
             if 0 <= inpIndexDeleteMahasiswa < n:
                 for i in range(inpIndexDeleteMahasiswa,n-1):
                     arrayNIMMahasiwa[i] = arrayNIMMahasiwa[i+1]
                     arrayNamaMahasiwa[i] = arrayNamaMahasiwa[i+1]
                 
-                arrayNIMMahasiwa[n-1] = None
+                arrayNIMMahasiwa[n-1]  = None
                 arrayNamaMahasiwa[n-1] = None
                 n-=1
                 print(f"Data pada Index{inpIndexDeleteMahasiswa} Berhasil Dihapus")
             else:
                 print("Data Gagal Dihapus")
 
+        # Delete From End
         case 6:
             if n > 0:
                 arrayNIMMahasiwa[n-1] = None
@@ -98,6 +109,7 @@ while True:
             else:
                 print("Index Gagal Dihapus")
 
+        # Delete First Occurrence 
         case 7:
             inpIndexMahasiswa = int(input("Masukkan NIM Index Sama: "))
             found = -1
@@ -118,11 +130,14 @@ while True:
                 print(f"Data pada Index {inpIndexMahasiswa} berhasil dihapus")
             else:
                 print("gagal Menghapus Data")
+
+        # Show Data
         case 8: 
             print("\nData Mahasiswa:")
             for i in range(n):
                 print(f"Index [{i}] {arrayNIMMahasiwa[i]} {arrayNamaMahasiwa[i ]}")
 
+        # Exit Program
         case 9:
             break
 
